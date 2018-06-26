b1796e6fdc14d56efdde4420bd5c6ca17b2e8cd0
Tushar Sah <sahtushar@gmail.com>
2018-06-26 12:15:01 +0530

    yhj

---
+++ PiedPiperIN/Views/Home/addtocart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using PiedPiperIN.Models;
+namespace PiedPiperIN.Views.Home
+{
+    public class addtocart
+    {
+        static void main(String[] strgs)
+        {
+            string connectionString = "";
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("addtocart", sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@id", qty.Text);
+               
+                sqlCmd.ExecuteNonQuery();
+
+                sqlCon.Close();
+
+                using (PiedPiperINEntities cartdb = new PiedPiperINEntities())
+                {
+
+                }
+            }
+        }
+    }
+}
